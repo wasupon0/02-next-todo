3603f3b84fc3f0d36b184f285474d2520d7b88d4
wasupon <wasupon.mail@gmail.com>
2024-01-09 15:44:05 +0900

    Mod: change h1 text to Todos2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
-        <h1 className="text-2xl">Todos</h1>
+        <h1 className="text-2xl">Todos2</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
           href="/new"
